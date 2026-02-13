6e17bd66a084d3060ade079a9e85f11e9733f2bf
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2026-02-13 13:45:47 -0500

    basic django app

---
+++ iuungo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from iuungo.users.models import User
+from iuungo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
