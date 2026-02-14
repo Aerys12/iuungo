517af1fb1bd258f01efb1d6669ba1da4790342ac
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2026-02-14 09:48:20 -0500

    download uv on startup

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+
+cd /home/site/wwwroot
+uv sync --frozen
+
 source /home/site/wwwroot/.venv/bin/activate
 python manage.py migrate --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2
